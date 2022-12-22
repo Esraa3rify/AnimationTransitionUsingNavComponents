f8c2ce2e0f84972aacd908e249b9554742e5f6fb
EsraaArify <earify@provensolution.com>
2022-12-22 01:45:11 -0800

    animationTransitionBtwFragments

---
+++ app/src/main/java/com/example/customanimationtransitionbetweenfragments/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.customanimationtransitionbetweenfragments
+
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
